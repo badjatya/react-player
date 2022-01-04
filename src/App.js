d505ab6d3f83e0204f9e5cb0f16dfe044cabe5b8
Archit Badjatya <en19cs304010@medicaps.ac.in>
2022-01-04 22:51:55 +0530

    Setting library status

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React, { useState, useRef } from "react";
 import Song from "./components/Song";
 import Player from "./components/Player";
 import Library from "./components/Library";
+import Nav from "./components/Nav";
 
 // Importing data
 import data from "./data";
@@ -16,9 +17,11 @@ const App = () => {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [libraryStatus, setLibraryStatus] = useState(false);
 
   return (
     <div className="App">
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
       <Library
         songs={songs}
         setCurrentSong={setCurrentSong}
---
+++ src/components/Nav.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// React Icons
+import { FaMusic } from "react-icons/fa";
+
+const Nav = ({ libraryStatus, setLibraryStatus }) => {
+  return (
+    <nav>
+      <h1>Waves</h1>
+
+      <button onClick={() => setLibraryStatus(!libraryStatus)}>
+        Library
+        <FaMusic />
+      </button>
+    </nav>
+  );
+};
+
+export default Nav;
